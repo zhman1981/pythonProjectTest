dedc8a37e0624ff5580c6f46bfd3f6a7d2f01827
zhman1981 <zhman@ukr.net>
2022-05-09 17:22:44 +0300

    добавлена реализация дополнительного задания

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import keyboard
+import json
 
-stop = 0 # переменнаяя для прерывания цикла в случае неправильного шага
+stop = False # переменнаяя для прерывания цикла в случае неправильного шага
 x = 0 # переменная для того, чтобы KEY_DOWN обрабалтывалась функцией, а KEY_UP нет
+y = False # переменная для отсечки нажатия клавиш, отличных от стрелок управления
 
 # добавил класс, хотя без него можно было обойтись, но в условии было сказано использовать ООП...
 class choise():
@@ -13,25 +15,30 @@ class choise():
 
 # функция обрабатывающая нажатие клавиши (управление Шариком осуществляется курсором)
 def step(key):
-    global x
+    global x, y
     x +=1
     if x%2 == 1:
         res = 0
-        k = float(i/2)
         if key == 'up':
-            res = route[int(k)].up
+            res = route[int(i)].up
         elif key == 'down':
-            res = route[int(k)].down
+            res = route[int(i)].down
         elif key == 'left':
-            res = route[int(k)].left
+            res = route[int(i)].left
         elif key == 'right':
-            res = route[int(k)].right
-        print(res_text[res])
+            res = route[int(i)].right
+        else:
+            print('\nДля управления Шариком используйте стрелки.\n')
         global stop
         if 2 <= res <= 4:
-            stop = 1
+            stop = True
         else:
-            stop = 0
+            stop = False
+        if res == 1 or res == 2 or res == 3 or res == 4:
+            print(res_text[res])
+            y = True
+        else:
+            y = False
 
 # правильный маршрут Шарика
 route = [choise(4,4,4,1),choise(2,1,3,2),choise(3,2,1,2),choise(2,1,2,3),
@@ -48,19 +55,40 @@ res_text = {1:'Шарик на верном пути',
             4:'Шарик заблудился',
             }
 
-
-print('Помогаем Шарику найти косточку.\nСделайте первый шаг.')
-
-for i in range(len(route)*2):
-    step(keyboard.read_key())
-    if stop == 1:
+print('Помогаем Шарику найти косточку.')
+s = 0
+with open('save.json', 'r') as read_file:
+    st = json.load(read_file)
+if st == {} or st == 0:
+    print('Сделайте первый шаг:')
+else:
+    save = input('Есть сохранённый результат предыдущей игры, начать игру с сохранённой позиции? (y/n).\n')
+    if save == 'y':
+        s = int(st)
+        print('Сделайте следующий шаг, после сохранения:')
+    else:
+        s = 0
+        print('Сделайте первый шаг:')
+for i in range(s,len(route)):
+    y = False
+    while y == False:
+        step(keyboard.read_key())
+    if stop == True:
         print('Игра окончена!')
+        yn = input('Хотите сохранить пройденный маршрут? (y/n)\n')
+        if yn == 'y':
+            with open('save.json', 'w') as write_file:
+                json.dump(i, write_file)
+        else:
+            with open('save.json', 'w') as write_file:
+                json.dump(0, write_file)
         break
-    elif stop == 0 and x%2 == 0:
+    elif stop == False and x%2 == 1:
         print('Сделайте следующий шаг:')
         continue
+if stop == False:
+    print('Поздравляем с победой!')
 
-print('Поздравляем с победой!')
 
 # 80% времени убил на разбор того, как считывать кнопки курсора... и всё равно выглядит как-то криво...
 # должен быть более простой метод считывания нажатия на python, буду благодарен, если подскажите мне его.
